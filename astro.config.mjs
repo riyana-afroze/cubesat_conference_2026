d8652688c7ed202dda447768e7facd087a2bcdcd
Riyana <rafroze@mun.ca>
2026-01-05 05:02:39 -0330

    / insted of /en

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
         defaultLocale: "en",
         locales: ["en", "fr"],
         routing: {
-            prefixDefaultLocale: true
+            prefixDefaultLocale: false
         }
     }
 });
